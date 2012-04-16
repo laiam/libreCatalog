08f06c01e55e11cb8147c169f9486e14e1c5f200
Stephen <laiam.van@gmail.com>
2012-04-15 22:44:30 -0500

    main menu

--- src/librecatalog/Main.java
+++ src/librecatalog/Main.java
@@ -19,12 +19,12 @@ public class Main
      * Initialize the program, setup the configuration file and load file databases
      * into active memory.
      * 
-     * @param args the command line arguments potentially allow for -no-gui arg.
+     * @param args the command line arguments potentially allow for --no-gui arg.
      */
     public static void main(String[] args)
     {
         /* ---Archaic invocations-------------------------------------------- */
-        systemStartup();
+        args = systemStartup(args);
         
         //assume gui enabled by default
         UserInterface.main(args);
@@ -33,19 +33,27 @@ public class Main
     /**
      * System startup
      */
-    private static void systemStartup()
+    private static String[] systemStartup(String[] args)
     {
         Configure conf = new Configure("config.properties");
-        if (!conf.firstRun)
-        {
-            Patron.load(Configure.getProp("PatronDB"));
-            //Item.load(Configure.getProp("ItemDB"));
-            //Fines.load(Configure.getProp("FineDB"));
-            //ItemAvailability.load(Configure.getProp("AvailabilityDB"));
+        if (args.length == 0) {
+            args    = new String[1];
+            args[0] = "--FirstRun";
+        } else {
+            boolean flagSet = false;
+            for (int idx=0;idx < args.length-1; idx++)
+                if (args[idx].equals("--FirstRun")||args[idx].equals("-F")) {
+                    flagSet=true;
+                    break;
+                }
+            if (!flagSet) {
+                String[] temp = new String[args.length+1];
+                System.arraycopy(args, 0, temp, 0, args.length);
+                temp[args.length]="--FirstRun";
+                args = temp;
+            }
         }
-        else
-        {
-            //UserInterface.firstRun();
-        }
+        return args;
+        
     }
 }
