14877047f6791935815a9e8b6b05adbfda18d8e9
Stephen <laiam.van@gmail.com>
2012-04-17 20:40:12 -0500

    conf

--- src/librecatalog/Configure.java
+++ src/librecatalog/Configure.java
@@ -8,23 +8,21 @@
  */
 package librecatalog;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 /**
- *
+ *  This conf class will eventually replace configure.
+ * I'm testing a different way of doing things.
  * @author van
  */
-class Configure
+public class Configure
 {
-    private static Properties config = new Properties();
-    static boolean firstRun = true;
-    private static int userLevel;
+    static LinkedList<Setting> settings = new LinkedList<Setting>();
+    static fileDB<Setting> SettingDB;
     
-    public static String[] main (String[] args)
+    static void main(String[] args)
     {
         String filename = "config.properties";
         if (args.length>0)
@@ -33,64 +31,70 @@ class Configure
                     filename = args[idx].split("=")[1];
                 }
         String path = getPath(filename);
-        try
-        {
-            FileInputStream propFile = new FileInputStream( path );
-            config.load(propFile);
-            firstRun=false;
-        }
-        catch (FileNotFoundException fnfe)
-        {
-            if (args.length == 0) {
-                args    = new String[1];
-                args[0] = "--first-run";
-            } else {
-                boolean flagSet = false;
-                for (int idx=0;idx < args.length; idx++)
-                    if (args[idx].equals("--first-run")||args[idx].equals("-F")) {
-                        flagSet=true;
-                        break;
-                    }
-                if (!flagSet) {
-                    String[] temp = new String[args.length+1];
-                    System.arraycopy(args, 0, temp, 0, args.length);
-                    temp[args.length]="--first-run";
-                    args = temp;
-                }
-            }
-            System.out.println("First run: or config file failure.");
-            UserInterface.Error(101);
-            if (UserInterface.productSetupKey())
-                createConfig(filename);
-            else
-                System.exit(0);
-        }
-        catch (IOException ioe)
-        {
-            System.out.println("unexpected error:");
-            ioe.printStackTrace(System.out);
-            UserInterface.Error(102);
-        }
-        return args;
-    }
-
-    private static void writeConfig(String path)
-    {
-        
-        try {
-            FileOutputStream propFile = new FileOutputStream( path );
-            config.store(propFile, "");
-        }
-        catch (FileNotFoundException fnfe)
-        {
-            UserInterface.Error(104);
-        }
-        catch (IOException ioe)
-        {
-            UserInterface.Error(104);
-        }
+        SettingDB = new fileDB<Setting>(path);
+        SettingDB.load(settings);
+        System.out.println(settings.size() + " settings loaded.");
+        if (settings.size()==0)
+            loadDefaults();
     }
     
+    static void unload() {
+        SettingDB.save(settings);
+    }
+    
+    static void loadDefaults() {
+        settings.add(new Setting("PatronDB", getPath("Patrons.dbflat")));
+        settings.add(new Setting("ItemDB", getPath("Items.dbflat")));
+        settings.add(new Setting("FineDB", getPath("Fines.dbflat")));
+        settings.add(new Setting("AvailabilityDB", getPath("ItemAvailability.dbflat")));
+        unload();
+    }
+    
+    static void addSetting (String key, String token) {
+        Setting temp;
+        Iterator conf = settings.iterator();
+        while (conf.hasNext()) {
+            temp = (Setting) conf.next();
+            if (temp.getKey().equals(key)) {
+                settings.remove(temp);
+                break;
+            }
+        }
+        settings.add(new Setting(key, token));
+    }
+    
+    static String getSetting (String key) {
+        Setting temp;
+        Iterator conf = settings.iterator();
+        while (conf.hasNext()) {
+            temp = (Setting) conf.next();
+            if (temp.getKey().equals(key)) {
+                return temp.getToken();
+            }
+        }
+        return "";
+    }
+    
+    static boolean removeSetting (String key) {
+        Setting temp;
+        Iterator conf = settings.iterator();
+        while (conf.hasNext()) {
+            temp = (Setting) conf.next();
+            if (temp.getKey().equals(key)) {
+                return settings.remove(temp);
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Get an absolute path for a file.
+     * Calculate an absolute path relative to the location of the .jar or class
+     * files.
+     * 
+     * @param filename the name of the file to access absolutely.
+     * @return a full absolute path.
+     */
     static String getPath(String filename)
     {
         String path = "";
@@ -106,26 +110,29 @@ class Configure
         }
         return path+filename;
     }
-
-    private static void createConfig(String filename)
-    {
-        String path = getPath(filename);
-        config.setProperty("PatronDB",Configure.getPath("Patrons.dbflat"));
-        config.setProperty("ItemDB",Configure.getPath("Items.dbflat"));
-        config.setProperty("FineDB",Configure.getPath("Fines.dbflat"));
-        config.setProperty("AvailabilityDB",Configure.getPath("ItemAvailability.dbflat"));
-        writeConfig(path);
+}
+class Setting implements Serializable {
+    private String key;
+    private String token;
+    
+    Setting () {
+        key = "";
+        token = "";
     }
     
-    public static String getSetting(String key) {
-        return config.getProperty(key);
+    public Setting (String key, String token) {
+        this.key = key;
+        this.token = token;
     }
-
-    static void setUserLevel(int value)
-    {
-        userLevel = value;
+    
+    public void getToken(String newToken) {
+        token = newToken;
     }
-    static int getUserLevel() {
-        return userLevel;
+    
+    public String getKey() {
+        return key;
+    }
+    public String getToken() {
+        return token;
     }
 }
