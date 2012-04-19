66056c2b5942786246e723822cd6f070fc9dcb7e
Stephen <laiam.van@gmail.com>
2012-04-19 00:44:41 -0500

    nogui

--- src/librecatalog/Main.java
+++ src/librecatalog/Main.java
@@ -27,7 +27,6 @@ public class Main
     public static void main(String[] args)
     {
         intro();
-        System.out.println(args[0]);
         /* ---Archaic invocations-------------------------------------------- */
         
         //<editor-fold defaultstate="collapsed" desc="UIManager Look and Feel" >
