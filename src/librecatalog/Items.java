3bd18e05aec1341eef050431d0cc0fd21301052e
Van <Van@Traveller.gateway.2wire.net>
2012-04-24 14:45:37 -0500

    Bug Squashing

--- src/librecatalog/Items.java
+++ src/librecatalog/Items.java
@@ -15,8 +15,8 @@ import java.util.LinkedList;
 class Items
 {
 
-    private static LinkedList<Item> items = new LinkedList<Item>();
-    private static FileOps<Item> ItemDB = new FileOps<Item>( Configure.getPath( Configure.getSetting( "ItemDB" ) ) );
+    private static LinkedList<Record> items = new LinkedList<Record>();
+    private static FileOps<Record> ItemDB = new FileOps<Record>( Configure.getPath( Configure.getSetting( "ItemDB" ) ) );
 
     static void main ( String[] args )
     {
@@ -33,16 +33,16 @@ class Items
     /**
      * Adds a item to the system.
      *
-     * @param record An instance of the Item class containing the record.
+     * @param record An instance of the Record class containing the record.
      *
      * @return boolean success of operation.
      */
-    public static boolean addItem ( Item record )
+    public static boolean addItem ( Record record )
     {
         return items.add( record );
     }
 
-    public static void replaceItem ( Item oldRecord, Item newRecord )
+    public static void replaceItem ( Record oldRecord, Record newRecord )
     {
         int position = items.indexOf( oldRecord );
         items.set( position, newRecord );
@@ -59,31 +59,31 @@ class Items
      *
      * @return an array of items matching the criteria.
      */
-    public static Item[] searchItems ( int type, String str )
+    public static Record[] searchItems ( int type, String str )
     {
         Iterator itemIterator = items.iterator();
-        LinkedList<Item> itemList = new LinkedList<Item>();
-        Item tempP;
-        Item[] tempArray;
+        LinkedList<Record> itemList = new LinkedList<Record>();
+        Record tempP;
+        Record[] tempArray;
         switch ( type )
         {
             case 1:
             {
                 while ( itemIterator.hasNext() )
                 {
-                    tempP = (Item) itemIterator.next();
+                    tempP = (Record) itemIterator.next();
                     if ( tempP.getBarcode().equals( str ) )
                     {
                         itemList.add( tempP );
                     }
-                    return (Item[]) itemList.toArray();
+                    return (Record[]) itemList.toArray();
                 }
                 break;
             }
             case 2:
                 while ( itemIterator.hasNext() )
                 {
-                    tempP = (Item) itemIterator.next();
+                    tempP = (Record) itemIterator.next();
                     if ( tempP.getTitle().equals( str ) )
                     {
                         itemList.add( tempP );
@@ -93,7 +93,7 @@ class Items
             case 3:
                 while ( itemIterator.hasNext() )
                 {
-                    tempP = (Item) itemIterator.next();
+                    tempP = (Record) itemIterator.next();
                     if ( tempP.getAuthor().equals( str ) )
                     {
                         itemList.add( tempP );
@@ -101,7 +101,7 @@ class Items
                 }
                 break;
         }
-        tempArray = new Item[itemList.size()];
+        tempArray = new Record[itemList.size()];
         for ( int idx = 0; idx < itemList.size(); idx++ )
         {
             tempArray[idx] = itemList.get( idx );
@@ -113,20 +113,20 @@ class Items
     /**
      * Removes a item from the system.
      *
-     * @param record An instance of the Item class containing the record to be
-     *               removed.
+     * @param record An instance of the Record class containing the record to be
+     * removed.
      *
      * @return boolean success of operation.
      */
-    public static boolean removeItem ( Item record )
+    public static boolean removeItem ( Record record )
     {
         return items.remove( record );
     }
 
     static String nextAvailableNumber ()
     {
-        Item lastItem = items.peekLast();
-        Item firstItem = items.peekLast();
+        Record lastItem = items.peekLast();
+        Record firstItem = items.peekLast();
         String barcode;
 
         if ( lastItem != null )
@@ -158,133 +158,162 @@ class Items
         }
         return "00000001";
     }
+
+    public static Record Record ( String barcode,
+                                  String itemTitle,
+                                  String itemAuthor,
+                                  String itemGenre,
+                                  String shelfLocation,
+                                  int year,
+                                  int month,
+                                  int day,
+                                  String keywords )
+    {
+        return new Record(barcode, itemTitle, itemAuthor, itemGenre, shelfLocation,
+                year, month, day, keywords);
+    }
+
+    static class Record implements Serializable
+    {//begin class Items
+
+        private String barcode,
+                Title,
+                Author,
+                Genre,
+                shelfLocation,
+                tagsString;
+        private String[] tags;
+        private int year, month, day;
+
+        Record ( String barcode,
+                 String itemTitle,
+                 String itemAuthor,
+                 String itemGenre,
+                 String shelfLocation,
+                 int year,
+                 int month,
+                 int day,
+                 String keywords )
+        {
+            //todo
+            //method to check for valid barcode
+            //method to check for valid shelf location/dewey decimal
+            this.barcode = barcode;
+            this.Title = itemTitle;
+            this.Author = itemAuthor;
+            this.Genre = itemGenre;
+            this.shelfLocation = shelfLocation;
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.tagsString = keywords;
+            this.tags = keywords.split( "," );
+        }
+
+        public String getAuthor ()
+        {
+            return Author;
+        }
+
+        public String getGenre ()
+        {
+            return Genre;
+        }
+
+        public String getTitle ()
+        {
+            return Title;
+        }
+
+        public String getBarcode ()
+        {
+            return barcode;
+        }
+
+        public String getDate () {
+            return month + "/" + day + "/" + year;
+        }
+        
+        public int getDay ()
+        {
+            return day;
+        }
+
+        public int getMonth ()
+        {
+            return month;
+        }
+
+        public String getShelfLocation ()
+        {
+            return shelfLocation;
+        }
+
+        public String getTagsString ()
+        {
+            return tagsString;
+        }
+        
+        public String[] getTags ()
+        {
+            return tags;
+        }
+
+        public int getYear ()
+        {
+            return year;
+        }
+
+        public void setAuthor ( String Author )
+        {
+            this.Author = Author;
+        }
+
+        public void setGenre ( String Genre )
+        {
+            this.Genre = Genre;
+        }
+
+        public void setTitle ( String Title )
+        {
+            this.Title = Title;
+        }
+
+        public void setBarcode ( String barcode )
+        {
+            this.barcode = barcode;
+        }
+
+        public void setDay ( int day )
+        {
+            this.day = day;
+        }
+
+        public void setMonth ( int month )
+        {
+            this.month = month;
+        }
+
+        public void setYear ( int year )
+        {
+            this.year = year;
+        }
+        
+        public void setDate (int y, int m, int d) {
+            this.year = y;
+            this.month = m;
+            this.day = d;
+        }
+
+        public void setShelfLocation ( String shelfLocation )
+        {
+            this.shelfLocation = shelfLocation;
+        }
+
+        public void setTags ( String tags )
+        {
+            this.tagsString = tags;
+            this.tags = tags.split( "," );
+        }
+    }//end class Items
 }
-
-class Item implements Serializable
-{//begin class Items
-
-    private String barcode,
-            Title,
-            Author,
-            Genre,
-            shelfLocation;
-    private String[] tags;
-    private int year, month, day;
-
-    Item ( String barcode,
-           String itemTitle,
-           String itemAuthor,
-           String itemGenre,
-           String shelfLocation,
-           int year,
-           int month,
-           int day,
-           String keywords )
-    {
-        //todo
-        //method to check for valid barcode
-        //method to check for valid shelf location/dewey decimal
-        this.barcode = barcode;
-        this.Title = itemTitle;
-        this.Author = itemAuthor;
-        this.Genre = itemGenre;
-        this.shelfLocation = shelfLocation;
-        this.year = year;
-        this.month = month;
-        this.day = day;
-        this.tags = keywords.split( "," );
-    }
-
-    public String getAuthor ()
-    {
-        return Author;
-    }
-
-    public String getGenre ()
-    {
-        return Genre;
-    }
-
-    public String getTitle ()
-    {
-        return Title;
-    }
-
-    public String getBarcode ()
-    {
-        return barcode;
-    }
-
-    public int getDay ()
-    {
-        return day;
-    }
-
-    public int getMonth ()
-    {
-        return month;
-    }
-
-    public String getShelfLocation ()
-    {
-        return shelfLocation;
-    }
-
-    public String[] getTags ()
-    {
-        return tags;
-    }
-
-    public int getYear ()
-    {
-        return year;
-    }
-
-    public void setAuthor ( String Author )
-    {
-        this.Author = Author;
-    }
-
-    public void setGenre ( String Genre )
-    {
-        this.Genre = Genre;
-    }
-
-    public void setTitle ( String Title )
-    {
-        this.Title = Title;
-    }
-
-    public void setBarcode ( String barcode )
-    {
-        this.barcode = barcode;
-    }
-
-    public void setDay ( int day )
-    {
-        this.day = day;
-    }
-
-    public void setMonth ( int month )
-    {
-        this.month = month;
-    }
-
-    public void setShelfLocation ( String shelfLocation )
-    {
-        this.shelfLocation = shelfLocation;
-    }
-
-    public void setTags ( String[] tags )
-    {
-        this.tags = tags;
-    }
-
-    public void setYear ( int year )
-    {
-        this.year = year;
-    }
-    
-    
-}//end class Items
-
