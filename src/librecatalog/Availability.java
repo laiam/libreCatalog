354d25d109ea5a1f187ddd300c9363cbda9b9e8a
Van <Van@Traveller.gateway.2wire.net>
2012-04-24 10:38:27 -0500

    Bug Squashing

--- src/librecatalog/Availability.java
+++ src/librecatalog/Availability.java
@@ -1,138 +1,135 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Name:       Team Innovation
+ * Course:     CS225
+ * Program:    Project Library
+ * Problem:    Create a system for storing library books and patrons, provide methods
+ *             for checking out books, and other library related tasks.
+ * Class:      Avilability
  */
 package librecatalog;
+
 import java.io.Serializable;
 import java.util.Iterator;
 import java.util.LinkedList;
+
 /**
  *
  * @author Charlie Kaden
  */
 class Availability
-{//begin of ItemAvailability
-    
-     private static LinkedList<ItemAvailability> ItemAvail =
-             new LinkedList<ItemAvailability>();
-    private static fileDB<ItemAvailability> ItemavailabilityDB =
-            new fileDB<ItemAvailability>(Configure.getSetting("ItemavailabilityDB"));
-    
+{//begin of Record
+
+    private static LinkedList<Record> ItemAvail =
+                                      new LinkedList<Record>();
+    private static fileDB<Record> ItemavailabilityDB =
+                                  new fileDB<Record>( Configure.getPath(Configure.getSetting( "AvailabilityDB" )) );
+
     /**
-     * 
-     * @param args 
+     *
+     * @param args
      */
-    static void main(String[] args)
+    static void main( String[] args )
     {//begin of main
-        ItemavailabilityDB.load(ItemAvail);
-        System.out.println(ItemAvail.size() + " ItemAvailability loaded.");
+        ItemavailabilityDB.load( ItemAvail );
+        System.out.println( ItemAvail.size() + " ItemAvailability loaded." );
     }//
 
-    static void unload() {
-        System.out.println("Unloading "+ItemAvail.size()+" ItemAvailability");
-        ItemavailabilityDB.save(ItemAvail);
+    static void unload()
+    {
+        System.out.println( "Unloading " + ItemAvail.size() + " ItemAvailability" );
+        ItemavailabilityDB.save( ItemAvail );
     }
-    
-     /**
+
+    /**
      * Adds a itemAvailability to the system.
-     * @param record  An instance of the Item class containing the record.
+     *
+     * @param record An instance of the Item class containing the record.
      * @return boolean success of operation.
      */
-    public static boolean addAvailability(ItemAvailability record) 
+    public static boolean addAvailability( Record record )
     {
-        return ItemAvail.add(record);
+        return ItemAvail.add( record );
     }
-    
-    public static void replaceAvailability(ItemAvailability oldRecord, ItemAvailability newRecord) 
+
+    public static void replaceAvailability( Record oldRecord, Record newRecord )
     {
-        int position = ItemAvail.indexOf(oldRecord);
-        ItemAvail.set(position, newRecord);
+        int position = ItemAvail.indexOf( oldRecord );
+        ItemAvail.set( position, newRecord );
     }
-    
-    
-     /**
+
+    /**
      * Search for items in the database.
      *
      * @param type type of search to perform.
-     *             1 - search based on DueDate.
-     *             2 - search based on Item and Patron bar code.
-     *             3 - search based on the date the record is created.
-     * @param str value to search for.
+     * 1 - search based on DueDate.
+     * 2 - search based on Item and Patron bar code.
+     * 3 - search based on the date the record is created.
+     * @param str  value to search for.
      * @return an array of items matching the criteria.
      */
-    public static ItemAvailability[] searchItems(int type, String str)
+    public static Record[] searchItems( int type, String str )
     {
         Iterator itemIterator = ItemAvail.iterator();
-        LinkedList<ItemAvailability> itemList = new LinkedList<ItemAvailability>();
-        ItemAvailability tempP;
-        ItemAvailability[] tempArray;
-        switch (type)
-        {
-            case 1:
-            {
-                while (itemIterator.hasNext())
-                {
-                    tempP = (ItemAvailability) itemIterator.next();
-                    if (tempP.getDueDate().equals(str))
-                    {
-                        itemList.add(tempP);
-                    }
-                    return (ItemAvailability[]) itemList.toArray();
+        LinkedList<Record> itemList = new LinkedList<Record>();
+        Record tempP;
+        Record[] tempArray;
+        switch ( type ) {
+            case 1: {
+                while ( itemIterator.hasNext() ) {
+                    tempP = (Record) itemIterator.next();
+                    if ( tempP.getDueDate().equals( str ) )
+                        itemList.add( tempP );
+                    return (Record[]) itemList.toArray();
                 }
                 break;
             }
             case 2:
-                while (itemIterator.hasNext())
-                {
-                    tempP = (ItemAvailability) itemIterator.next();
-                    if (tempP.getPatronBarcode().equals(str))
-                    {
-                        itemList.add(tempP);
-                    }
-                    else if(tempP.getItemBarcode().equals(str))
-                    {
-                        itemList.add(tempP);
-                    }
+                while ( itemIterator.hasNext() ) {
+                    tempP = (Record) itemIterator.next();
+                    if ( tempP.getPatronBarcode().equals( str ) )
+                        itemList.add( tempP );
+                    else if ( tempP.getItemBarcode().equals( str ) )
+                        itemList.add( tempP );
                 }
                 break;
             case 3:
-                while (itemIterator.hasNext())
-                {
-                    tempP = (ItemAvailability) itemIterator.next();
-                    if (tempP.getRecordCreated().equals(str))
-                    {
-                        itemList.add(tempP);
-                    }
+                while ( itemIterator.hasNext() ) {
+                    tempP = (Record) itemIterator.next();
+                    if ( tempP.getRecordCreated().equals( str ) )
+                        itemList.add( tempP );
                 }
                 break;
         }
-        tempArray = new ItemAvailability[itemList.size()];
-        for (int idx = 0; idx < itemList.size();idx++)
-            tempArray[idx]=itemList.get(idx);
-        return tempArray;}
-        
-        
-     /**
-     * Removes a item from the system.
-     * @param record An instance of the Item class containing the record to be removed.
-     * @return boolean success of operation.
-     */
-    public static boolean removeItemavailability(Item record)
-    {
-        return ItemAvail.remove(record);
+        tempArray = new Record[itemList.size()];
+        for ( int idx = 0; idx < itemList.size(); idx++ ) {
+            tempArray[idx] = itemList.get( idx );
+        }
+        return tempArray;
     }
 
-    
-}
-    class ItemAvailability implements Serializable
-    {//begin class Items
-    private String itemBarcode,
-                   patronBarcode,
-                   DueDate,
-                   RecordCreated;
-    private int typeOf;
+    /**
+     * Removes a item from the system.
+     *
+     * @param record An instance of the Item class containing the record to be
+     * removed.
+     * @return boolean success of operation.
+     */
+    public static boolean removeRecord( Record record )
+    {
+        return ItemAvail.remove( record );
+    }
 
-        public ItemAvailability(String itemBarcode, String patronBarcode, String DueDate, String RecordCreated, int typeOf) {
+    class Record implements Serializable
+    {//begin class Items
+
+        private String itemBarcode,
+                patronBarcode,
+                DueDate,
+                RecordCreated;
+        private int typeOf;
+
+        public Record( String itemBarcode, String patronBarcode, String DueDate, String RecordCreated, int typeOf )
+        {
             this.itemBarcode = itemBarcode;
             this.patronBarcode = patronBarcode;
             this.DueDate = DueDate;
@@ -140,45 +137,54 @@ class Availability
             this.typeOf = typeOf;
         }
 
-    public String getDueDate() {
-        return DueDate;
-    }
+        public String getDueDate()
+        {
+            return DueDate;
+        }
 
-    public void setDueDate(String DueDate) {
-        this.DueDate = DueDate;
-    }
+        public void setDueDate( String DueDate )
+        {
+            this.DueDate = DueDate;
+        }
 
-    public String getRecordCreated() {
-        return RecordCreated;
-    }
+        public String getRecordCreated()
+        {
+            return RecordCreated;
+        }
 
-    public void setRecordCreated(String RecordCreated) {
-        this.RecordCreated = RecordCreated;
-    }
+        public void setRecordCreated( String RecordCreated )
+        {
+            this.RecordCreated = RecordCreated;
+        }
 
-    public String getItemBarcode() {
-        return itemBarcode;
-    }
+        public String getItemBarcode()
+        {
+            return itemBarcode;
+        }
 
-    public void setItemBarcode(String itemBarcode) {
-        this.itemBarcode = itemBarcode;
-    }
+        public void setItemBarcode( String itemBarcode )
+        {
+            this.itemBarcode = itemBarcode;
+        }
 
-    public String getPatronBarcode() {
-        return patronBarcode;
-    }
+        public String getPatronBarcode()
+        {
+            return patronBarcode;
+        }
 
-    public void setPatronBarcode(String patronBarcode) {
-        this.patronBarcode = patronBarcode;
-    }
+        public void setPatronBarcode( String patronBarcode )
+        {
+            this.patronBarcode = patronBarcode;
+        }
 
-    public int getTypeOf() {
-        return typeOf;
-    }
+        public int getTypeOf()
+        {
+            return typeOf;
+        }
 
-    public void setTypeOf(int typeOf) {
-        this.typeOf = typeOf;
-    }
-    
-    
-}//end of ItemAvailability
+        public void setTypeOf( int typeOf )
+        {
+            this.typeOf = typeOf;
+        }
+    }//end of Record
+}
